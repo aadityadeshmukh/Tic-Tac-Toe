50074a5beb8d6827fa3136d8e46dcbfc76eaef2b
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-08-04 14:26:23 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello!')
+console.log('hello!');
+function blockSelect(element) {
+  console.log(element);
+  if (element.innerHTML !== 'X') element.innerHTML = 'X';
+  else console.log('Select again!');
+}
