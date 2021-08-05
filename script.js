ae23d2f12de97fe4c04d7d6479e42daa0e03768b
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-08-05 16:33:36 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 console.log('hello!');
 function blockSelect(element) {
   console.log(element);
-  if (element.innerHTML !== 'X') element.innerHTML = 'X';
-  else console.log('Select again!');
+  if (element.innerHTML !== 'X') {
+    let innerPara = document.createElement('p');
+    innerPara.setAttribute('class', 'innerPara');
+    innerPara.innerHTML = 'X';
+    element.appendChild(innerPara);
+  } else console.log('Select again!');
 }
