15eaba41af612430a77acd122730c3f9c51bdb63
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-08-06 18:03:36 +0530

    1 file modified

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ function blockSelect(element) {
   if (element.innerHTML !== 'X') {
     let innerPara = document.createElement('p');
     innerPara.setAttribute('class', 'innerPara');
-    if (clickCount % 2 == 0) innerPara.innerHTML = 'O';
-    else innerPara.innerHTML = 'X';
+    if (clickCount % 2 == 0) {
+      innerPara.innerHTML = 'O';
+      innerPara.style.color = 'red';
+    } else {
+      innerPara.innerHTML = 'X';
+      innerPara.style.color = 'blue';
+    }
     element.appendChild(innerPara);
   } else console.log('Select again!');
 }
