2f5d9937fa57c263debbe15ba0eacf7571a09a6d
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-08-07 11:44:18 +0530

    2 files modified

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let clickCount = 0;
 function blockSelect(element) {
   clickCount++;
   console.log(element);
-  if (element.innerHTML !== 'X') {
+  if (element.innerHTML !== 'X' && element.innerHTML !== 'O') {
     let innerPara = document.createElement('p');
     innerPara.setAttribute('class', 'innerPara');
     if (clickCount % 2 == 0) {
       innerPara.innerHTML = 'O';
-      innerPara.style.color = 'red';
+      innerPara.style.color = '#D4D2A5';
     } else {
       innerPara.innerHTML = 'X';
-      innerPara.style.color = 'blue';
+      innerPara.style.color = '#928779';
     }
     element.appendChild(innerPara);
   } else console.log('Select again!');
