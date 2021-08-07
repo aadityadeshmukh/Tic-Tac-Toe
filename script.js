271cb5eb67a413639f19a07e3702c924e5e916fc
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-08-07 11:47:22 +0530

    1 file modified

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let clickCount = 0;
 function blockSelect(element) {
   clickCount++;
   console.log(element);
-  if (element.innerHTML !== 'X' && element.innerHTML !== 'O') {
+  if (element.innerHTML !== 'X') {
     let innerPara = document.createElement('p');
     innerPara.setAttribute('class', 'innerPara');
     if (clickCount % 2 == 0) {
