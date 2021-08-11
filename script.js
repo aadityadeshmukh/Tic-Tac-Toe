8747fda37b1bf60a61934b7a1f5aec7d1a840e60
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-08-11 14:57:49 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 console.log('hello!');
+const Deck = () => {
+  let selection = {};
+  const addSelection = selection => {
+    console.log(selection);
+  };
+  return { addSelection };
+};
+let playDeck = Deck();
 let clickCount = 0;
 function blockSelect(element) {
   clickCount++;
@@ -14,11 +22,16 @@ function blockSelect(element) {
       innerPara.style.color = '#928779';
     }
     element.appendChild(innerPara);
+    let selectedElem = element.getAttribute('data-griditem');
+    playDeck.addSelection(selectedElem);
   } else console.log('Select again!');
 }
+
 const Player = name => {
   const getName = () => name;
   return { getName };
 };
 let human = Player('Wretched Human');
+let computer = Player('Silicon God');
 console.log(human.getName());
+console.log(computer.getName());
