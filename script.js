d32df9bbf0c310f0eb5e8e241f45ea4877f49500
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-08-12 18:33:21 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 console.log('hello!');
 //Deck module
 const Deck = () => {
+  let availablePlaces = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let selection = [];
   const addSelection = selected => {
     console.log(selected);
     selection.push(selected);
-    console.log(selection);
+    console.log(typeof selected);
+    let idToRem = availablePlaces.indexOf(parseInt(selected));
+    availablePlaces.splice(idToRem, 1);
+    console.log(availablePlaces);
   };
   return { addSelection };
 };
